2ca2558b66a007837abdb19279d844d5fccaf229
Benoît Legat <benoit.legat@gmail.com>
2021-10-02 23:05:25 -0400

    Finish hybrid controller example

--- docs/src/examples/hybrid_controlled.jl
+++ docs/src/examples/hybrid_controlled.jl
@@ -1,4 +1,93 @@
 using Test     #src
+# # Continuous-time Controlled Invariant Set
+#
+#md # [![Binder](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/generated/continuous_controlled.ipynb)
+#md # [![nbviewer](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/generated/continuous_controlled.ipynb)
+#
+# ## Introduction
+#
+# This example considers the hybrid constrained linear control system:
+# ```math
+# \begin{aligned}
+# \dot{x}_1(t) & = x_2(t)\\
+# \dot{x}_2(t) & = u(t)
+# \end{aligned}
+# ```
+# with state constraint $x \in [-1, 1]^2$ and input constraint $u \in [-1, 1]$
+# and the jump:
+# ```math
+# \begin{aligned}
+# \dot{x}_1(t) & = -x_1(t) + u(t)/8\\
+# \dot{x}_2(t) & = x_2(x) - u(t)/8
+# \end{aligned}
+# ```
+# with state constraint $x \in [-1, 1]^2$ and input constraint $u \in [-1, 1]$
+# that can occur anytime.
+#
+# In order to compute controlled invariant sets for this system, we consider
+# the projection onto the first two dimensions of controlled invariant sets of the
+# following lifted system:
+# ```math
+# \begin{aligned}
+# \dot{x}_1(t) & = x_2(t)\\
+# \dot{x}_2(t) & = x_3(t)\\
+# \dot{x}_3(t) & = u(t)
+# \end{aligned}
+# ```
+# with state constraint $x \in [-1, 1]^3$;
+# with a first jump to a temporary mode:
+# ```math
+# \begin{aligned}
+# x_1^+ & = x_1\\
+# x_2^+ & = x_2\\
+# x_3^+ & = u
+# \end{aligned}
+# ```
+# with state constraint $x \in [-1, 1]^3$ and unconstrained input;
+# and a second jump back to the original mode:
+# ```math
+# \begin{aligned}
+# x_1^+ & = -x_1 + x_3/8\\
+# x_2^+ & = x_2 - x_3/8\\
+# x_3^+ & = u.
+# \end{aligned}
+# ```
+# Note that the input `u` chosen in the first jump is the input that will be used for
+# the reset map and the input `u` chosen for the second jump is the input that will be used
+# for the state `x_3` of the continuous-time system.
+#
+# The matricial form of this system is given by $\dot{x}(t) = Ax(t) + Bu(t)$ where `A` and `B` are as defined below.
+# As shown in Proposition 5 of [LJ21], a set is controlled invariant for this system if and only if it is invariant for the algebraic system
+# ```math
+# \begin{aligned}
+# \dot{x}_1(t) & = x_2(t)\\
+# \dot{x}_2(t) & = x_3(t)
+# \end{aligned}
+# ```
+# with state constraint $x \in [-1, 1]^3$;
+# with a first jump to a temporary mode:
+# ```math
+# \begin{aligned}
+# x_1^+ & = x_1\\
+# x_2^+ & = x_2
+# \end{aligned}
+# ```
+# with state constraint $x \in [-1, 1]^3$
+# and a second jump back to the original mode:
+# ```math
+# \begin{aligned}
+# x_1^+ & = -x_1 + x_3/8\\
+# x_2^+ & = x_2 - x_3/8.
+# \end{aligned}
+# ``
+#
+# The matricial form of this system is given by $E\dot{x}(t) = Cx(t)$ with a first jump
+# $Ex^+ = Ex$ and a second jump $Ex^+ = Ux$ where:
+#
+# [LJ21] B. Legat and R. M. Jungers.
+# *Continuous-time controlled invariant sets, a geometric approach*.
+# 7th IFAC Conference on Analysis and Design of Hybrid Systems ADHS 2021, **2021**.
+
 
 A = [0.0 1.0 0.0
      0.0 0.0 1.0
@@ -47,16 +136,17 @@ interval = HalfSpace([1.0], 1.0) ∩ HalfSpace([-1.0], 1.0)
 lib = Polyhedra.DefaultLibrary{Float64}(lp_solver)
 □_2 = polyhedron(interval * interval, lib)
 □_3 = □_2 * interval
-dirs = [[-1 + √3, -1 + √3], [-1 + √3, 1 - √3]]
+u_max = 1/8
+x2l0 = √(2u_max) - u_max
+dirs = [[-1 + √3, -1 + √3], [-1/2, 1.0], [-1.0, x2l0]]
 all_dirs = [dirs; (-).(dirs)]
 inner = polyhedron(vrep(all_dirs), lib)
 outer = polar(inner)
 
 # ## Ellipsoidal template
 #
-# We start with the ellipsoidal template. We consider two different objectives (see Section 4.2 of [L20]):
-# * the volume of the set (which corresponds to $\log(\det(Q))$ or $\sqrt[n]{\det(Q)}$ in the objective function) and
-# * the sum of the squares of the length of its semi-axes of the polar (which corresponds to the trace of $Q$ in the objective function).
+# We start with the ellipsoidal template. The objective consider is to maximize `γ`
+# such that `γ * inner` is included in the set.
 
 sol_ell, γ_ell = maximal_invariant(Ellipsoid(symmetric=true))
 
@@ -75,7 +165,9 @@ cols = [canard, frambo]
 
 x2 = range(0, stop=1, length=20)
 x1 = 1 .- x2.^2 / 2
-upper = [[[-1, 1]]; [[x1[i], x2[i]] for i in eachindex(x2)]]
+x2l = range(x2l0, stop=1.0 - u_max, length=20)
+x1l = -(1 .- (x2l .+ u_max).^2 / 2 .+ u_max)
+upper = [[[-1/2, 1.0]]; [[x1l[i], x2l[i]] for i in eachindex(x2l)]; [[x1[i], x2[i]] for i in eachindex(x2)]]
 mci = polyhedron(vrep([upper; (-).(upper)]), lib)
 polar_mci = polar(mci)
 
@@ -106,11 +198,41 @@ polar_plot(project(sol_ell, 1:2), γ_ell)
 
 # ## Polyset template
 
-p4, γ4 = maximal_invariant(PolySet(symmetric=true, degree=4, convex=true), 0.71)
+# We start with quartic polynomials:
+
+p4, γ4 = maximal_invariant(PolySet(symmetric=true, degree=4, convex=true), 0.896)
 γ4
 
-p6, γ6 = maximal_invariant(PolySet(symmetric=true, degree=6, convex=true), 0.73)
+# Below is the primal plot:
+
+primal_plot(project(p4, 1:2), γ4)
+
+# and here is the polar plot:
+
+polar_plot(project(p4, 1:2), γ4)
+
+# We now try it with sextic polynomials:
+
+p6, γ6 = maximal_invariant(PolySet(symmetric=true, degree=6, convex=true), 0.93)
 γ6
 
-p8, γ8 = maximal_invariant(PolySet(symmetric=true, degree=6, convex=true), 0.73)
+# Below is the primal plot:
+
+primal_plot(project(p6, 1:2), γ6)
+
+# and here is the polar plot:
+
+polar_plot(project(p6, 1:2), γ6)
+
+# We now try it with octic polynomials:
+
+p8, γ8 = maximal_invariant(PolySet(symmetric=true, degree=8, convex=true), 0.96)
 γ8
+
+# Below is the primal plot:
+
+primal_plot(project(p8, 1:2), γ8)
+
+# and here is the polar plot:
+
+polar_plot(project(p8, 1:2), γ8)
